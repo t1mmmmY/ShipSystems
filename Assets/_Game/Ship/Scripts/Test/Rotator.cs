ce9b9fb0335f021e8860702880f4596ac6895344
t1mmmmY <t1mmmmY111@gmail.com>
2020-05-01 17:15:44 +0200

    HDRP assets

---
+++ Assets/_Game/Ship/Scripts/Test/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
